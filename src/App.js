db1441aaf989e79c4f02bf0a20e9ddd30a9cb646
vjiji <restmi0711@gmail.com>
2024-04-02 20:36:52 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import SockJS from "sockjs-client";
+import { Stomp } from "@stomp/stompjs";
 
 function App() {
+  const [stompClient, setStompClient] = useState(null);
+  const [name, setName] = useState("");
+  const [receivedMessage, setReceivedMessage] = useState(null);
+
+  useEffect(() => {
+    const socket = new SockJS("http://localhost:8080/ws");
+    const stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+      console.log("Connected: " + frame);
+      stompClient.subscribe("/topic/greetings", function (greeting) {
+        setReceivedMessage(JSON.parse(greeting.body).content);
+      });
+    });
+    setStompClient(stompClient);
+    return () => {
+      if (stompClient !== null) {
+        stompClient.disconnect();
+      }
+    };
+  }, []);
+
+  const sendMessage = () => {
+    stompClient.send("/app/hello", {}, JSON.stringify({ name: name }));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Enter your name"
+      />
+      <button onClick={sendMessage}>Send</button>
+      {receivedMessage && <div>Message: {receivedMessage}</div>}
     </div>
   );
 }
